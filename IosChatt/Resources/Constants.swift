7cb89e44afc819c7c8c7a6f5a133e2b2b6b36eeb
Marcus Nilsson <marcus.nilsson@yahoo.com>
2021-05-21 20:39:37 +0200

    Integrated login and registration functionallity with firebase

---
+++ IosChatt/Resources/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  IosChat
+//
+//  Created by Marcus Nilsson on 2021-05-21.
+//
+
+import Foundation
---
+++ IosChatt/Resources/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  IosChat
+//
+//  Created by Marcus Nilsson on 2021-05-21.
+//
+
+import Foundation
